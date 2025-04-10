1f39ec45f748ad4675f08fd8d5b320a0eb06bbed
Subhadro Samaddar <subhadrosamaddar@gmail.com>
2025-04-10 21:27:56 +0530

    current commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 const express = require('express');
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 const data = require('./cars.json'); // your JSON file
 
 app.get('/api/data', (req, res) => {
-    res.json(data);
+    res.send(data);
 });
 
 app.listen(PORT, () => {
